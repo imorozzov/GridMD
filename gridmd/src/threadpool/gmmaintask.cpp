790fcc55204729359f0053ebb296a51e12728d4a
Ivan-Grebennikov <gre-ivan@yandex.ru>
2016-03-21 02:53:09 +0300

    [fix] add instaged threadpool files

---
+++ gridmd/src/threadpool/gmmaintask.cpp
@@ -0,0 +1,24 @@
+#include "threadpool/gmmaintask.h"
+#include "gridmd.h"
+
+
+gmMainTask::gmMainTask(int (*gridmd_main)(int, char *[]), int argc, char *argv[]):
+    mFuncPtr(gridmd_main),
+    mArgc(argc),
+    mArgv(argv)
+{;}
+
+gmTask::gmTASK_TYPE gmMainTask::Type() const
+{
+    return gmTASK_GRIDMD_MAIN;
+}
+
+int gmMainTask::Run()
+{
+    try
+    {
+       mFuncPtr(mArgc, mArgv);
+    }
+    catch(const task_finished& ){;}
+    return 0;
+}
